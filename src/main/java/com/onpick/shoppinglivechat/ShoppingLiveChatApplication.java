f8a9835ef1aa289e9ff20591a40d0643caa57ddf
milkaisgoody <ehwhgod0712@gmail.com>
2026-01-21 11:44:17 +0900

    Initial commit

---
+++ src/main/java/com/onpick/shoppinglivechat/ShoppingLiveChatApplication.java
@@ -0,0 +1,13 @@
+package com.onpick.shoppinglivechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingLiveChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingLiveChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onpick/shoppinglivechat/ShoppingLiveChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onpick.shoppinglivechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingLiveChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
